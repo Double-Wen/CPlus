66e2674bfd49f5d6a38f942984c978b106eeb95b
DoubleWen <tcliuwenwen@126.com>
2020-04-29 19:39:36 +0800

    test use my own id

--- base/hello.cpp
+++ base/hello.cpp
@@ -5,3 +5,27 @@ int main()
     cout << "Hello World" << endl;
     return 0;
 }
+/*
+ * Filename: /home/ubuntu/Code/CPlus/base/hello.cpp
+ * Path: /home/ubuntu/Code/CPlus/base
+ * Created Date: Tuesday, April 28th 2020, 3:51:45 pm
+ * Author: ubuntu
+ * 
+ * Copyright (c) 2020 Your Company
+ */
+/*
+ * Filename: /home/ubuntu/Code/CPlus/base/hello.cpp
+ * Path: /home/ubuntu/Code/CPlus/base
+ * Created Date: Tuesday, April 28th 2020, 3:51:45 pm
+ * Author: liuwenwen
+ * 
+ * Copyright (c) 2020 ZSTU
+ */
+/*
+ * Filename: /home/ubuntu/Code/CPlus/base/hello.cpp
+ * Path: /home/ubuntu/Code/CPlus/base
+ * Created Date: Tuesday, April 28th 2020, 3:51:45 pm
+ * Author: liuwenwen
+ * 
+ * Copyright (c) 2020 yuhai.tech
+ */
