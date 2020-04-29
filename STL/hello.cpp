a94e61238eefd12ca1e0c4bf5402441f085508a0
DoubleWen <tcliuwenwen@126.com>
2020-04-29 19:33:38 +0800

    test fileheader

--- STL/hello.cpp
+++ STL/hello.cpp
@@ -1,3 +1,11 @@
+/*
+ * Filename: /home/ubuntu/Code/CPlus/STL/hello.cpp
+ * Path: /home/ubuntu/Code/CPlus/STL
+ * Created Date: Tuesday, April 28th 2020, 1:48:31 pm
+ * Author: ubuntu
+ * 
+ * Copyright (c) 2020 Your Company
+ */
 int main()
 {
     return 0;
--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,11 @@
+/*
+ * Filename: /home/ubuntu/Code/CPlus/hello.cpp
+ * Path: /home/ubuntu/Code/CPlus
+ * Created Date: Friday, April 24th 2020, 5:57:34 pm
+ * Author: ubuntu
+ * 
+ * Copyright (c) 2020 Your Company
+ */
 #include <iostream>
 using namespace std;
 int main(int argc, char *argv[])
