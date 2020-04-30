5f3292ef98511fd6f242e9f6a719a279c7f2159a
DoubleWen <tcliuwenwen@126.com>
2020-04-30 21:12:30 +0800

    after test, if the input length bigger than the second argument of cin.getline, the stdin will be actted like filled with \n;

--- base/stdin.cpp
+++ base/stdin.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 int main()
 {
-    char a[100];
+    char a[10];
     for(int i=0; i<3; i++)
     {
-        cin.getline(a, 100);
+        cin.getline(a, 10);
         cout<<a<<endl;
     }
     string b;
@@ -15,5 +15,6 @@ int main()
         getline(cin, b, '\n');
         cout << b << endl;
     }
+    cout << "end normally" << endl;
     return 0;
 }
